2fe8f8bbab166157ac44db852b0f03a75b39a7a6
VasiliyMac <cartario@yandex.ru>
2020-09-06 16:54:27 +0300

    validation

--- src/components/create-session.jsx
+++ src/components/create-session.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Formik} from 'formik';
+import * as yup from 'yup';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
@@ -9,81 +11,121 @@ const Wrapper = styled.div`
   margin: 10px auto;
 `;
 
-export default class extends React.Component {
-  constructor(props){
-    super(props);
+const ErrorField = styled.div`
+  font-size: 14px;
+  color: red;
 
-    this.state = {
-      id: 100,
-      type: `Велосипед`,
-      distance: ``,
-      comment: ``,
-      date: ``,
-    };
-    this._changeHandler = this._changeHandler.bind(this);
-    this._submitHandler = this._submitHandler.bind(this);    
+  @keyframes shake {
+    0%,
+    100% {
+      transform: translateX(0);
+    }
+  
+    10%,
+    30%,
+    50%,
+    70%,
+    90% {
+      transform: translateX(-5px);
+    }
+  
+    20%,
+    40%,
+    60%,
+    80% {
+      transform: translateX(5px);
+    }
   }
 
-  _submitHandler(e){
-    e.preventDefault();
-    this.props.createSession({
-      id: getRandomInt(1,100),
-      type: this.state.type,
-      distance: this.state.distance,
-      comment: this.state.comment,
-      date: new Date (this.state.date),
-    })    
-  }
+  animation: shake 0.6s;  
+`;
 
-  _changeHandler(e){
-    const name= e.target.name;    
-    this.setState({
-      [name]: e.target.value,
-    })    
-  }
+const initialValues = {
+  distance: ``,
+  type: ``,
+  date: ``,
+  comment: ``,
+};
 
-  render() {    
-    return (
-      <Wrapper>
+const validationSchema = yup.object({
+  comment: yup
+    .string()
+    .required(`Это поле обязательное`)
+    .min(5, `Минимум 5 символов`)
+    .max(255, `Достаточно)`),
+  type: yup.string().required(`Это поле обязательное`),
+  date: yup.date().required(`Это поле обязательное`),
+  distance: yup 
+    .number().required(`Это поле обязательное`).min(0, `Должно быть больше нуля`).max(100, `Это уже перебор`)
+});
+
+export default ({createSession}) => {
+  return (
+    <Wrapper>
         <Link to="/">Вернуться назад</Link>
         <h2>Новая тренировка</h2>
-        <Form onSubmit={this._submitHandler}>
-          <FormGroup>
-            <Label for="type">Type</Label>
-            <Input type="select" name="type" id="type" defaultValue={this.state.type} onChange={this._changeHandler}>
-              <option>Велосипед</option>
-              <option>Бег</option>
-              <option>Лыжи</option>
-              <option>Плавание</option>
-              <option>Ходьба</option>
-            </Input>
-          </FormGroup>          
-          <FormGroup>
-            <Label for="distance">Distance, km</Label>
-            <Input type="number" min="0" name="distance" id="distance" placeholder="distance" required
-            value={this.state.distance} onChange={this._changeHandler}/>
-          </FormGroup>
-          <FormGroup>
-            <Label for="exampleDate">Date</Label>
-            <Input
-              type="date"
-              name="date"
-              id="exampleDate"
-              placeholder="date placeholder"
-              value={this.state.date}
-              onChange={this._changeHandler}
-              required
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="comment">Comment</Label>
-            <Input type="textarea" name="comment" id="comment" 
-            value={this.state.comment} onChange={this._changeHandler}
-            />
-          </FormGroup>
-          <Button color="success">Добавить тренировку</Button>
-        </Form>
+        <Formik 
+          initialValues={initialValues}
+          validationSchema = {validationSchema}
+          onSubmit={(data, {setSubmitting, resetForm})=>{
+            setSubmitting(true);
+
+            createSession({
+              id: getRandomInt(1,100),
+              type: data.type,
+              distance: data.distance,
+              comment: data.comment,
+              date: new Date (data.date),
+            });
+
+            setSubmitting(false);
+            resetForm();
+        }}>
+          {({values, errors, touched, isSubmitting, handleChange, handleBlur, handleSubmit})=>(
+            <Form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label for="type">Type</Label>
+              <Input type="select" name="type" id="type" value={values.type} onChange={handleChange} onBlur={handleBlur}
+              valid={!errors.type && touched.type}
+              >
+                <option>Велосипед</option>
+                <option>Бег</option>
+                <option>Лыжи</option>
+                <option>Плавание</option>
+                <option>Ходьба</option>
+              </Input>
+              {touched.type && errors.type && <ErrorField>{errors.type}</ErrorField>}
+            </FormGroup>          
+            <FormGroup>
+              <Label for="distance">Distance, km</Label>
+              <Input type="number" name="distance" id="distance" placeholder="distance"
+              value={values.distance} onChange={handleChange} onBlur={handleBlur} valid={!errors.distance && touched.distance}/>
+              {touched.distance && errors.distance && <ErrorField>{errors.distance}</ErrorField>}
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleDate">Date</Label>
+              <Input
+                type="date"
+                name="date"
+                id="exampleDate"
+                placeholder="date placeholder"
+                value={values.date} onChange={handleChange} onBlur={handleBlur}
+                valid={!errors.date && touched.date}
+              />
+              {touched.date && errors.date && <ErrorField>{errors.date}</ErrorField>}
+            </FormGroup>
+            <FormGroup>
+              <Label for="comment">Comment</Label>
+              <Input type="textarea" name="comment" id="comment" 
+              value={values.comment} errors={errors.comment} onChange={handleChange} onBlur={handleBlur}
+              valid={!errors.comment && touched.comment}
+              />
+              {touched.comment && errors.comment && <ErrorField>{errors.comment}</ErrorField>}
+            </FormGroup>
+            <Button color="success" disabled={isSubmitting}>Добавить тренировку</Button>
+          </Form>
+          )}
+        </Formik>
       </Wrapper>
-    );
-  }
+  );
 };
